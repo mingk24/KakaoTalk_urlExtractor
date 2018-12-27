a0fa459f1a9817e20d84500a369260e9e544af45
Kyungkoo Min <mingk24@gmail.com>
2018-12-27 16:39:44 +0900

    Add files via upload

--- kakao_dump.py
+++ kakao_dump.py
@@ -8,20 +8,30 @@ ps) 이해를 돕기위해 디버그용 프린트문은 남겨놓음
 '''
 
 # 입출력 파일이름은 하드코딩 -.-a
+import msvcrt as m
+def wait():
+    m.getch()
+
 f_in  = open("KakaoTalkChats.txt", 'rt', encoding='UTF8')
 f_out = open("KakaoTalkChats_res.txt", 'wt', encoding='UTF8' )
+
+g_date = ""
+g_author =""
 
 while True:
     line = f_in.readline()
 
     if not line: break
 
-    #print ('INPUT = ['+line+']')
+    print ('INPUT = ['+line+']')
     idx = line.find(',')
     if (idx == -1):
         #continue
         if (line.find('http') != -1):
             html_url = line.strip()
+            print ('RESULT=', html_url)
+            f_out.write(g_date+'\t'+g_author+'\t'+' '+'\t'+html_url+'\n')
+            
             #print (html_url)
         else:            
             #print('Skip...\n')
@@ -38,6 +48,7 @@ while True:
 
         if (idx2 == -1 ):
             author = ""
+            continue   # 2017년 10월 10일 오후 5:03, 이상협과장 UT팀님이 박미령님을 초대했습니다.
         else: 
             author = remains[0: idx2]
             data = remains[idx2+1:].strip() 
@@ -58,9 +69,12 @@ while True:
         # print ('html_message='+html_message)
         # print ('html_url    ='+html_url)
 
-    print ('RESULT=', date, author, html_message, html_url)
-    
-    f_out.write(date+'\t'+author+'\t'+html_message+'\t'+html_url+'\n')
+        print ('RESULT=', date, author, html_message, html_url)
+        f_out.write(date+'\t'+author+'\t'+html_message+'\t'+html_url+'\n')
+
+    g_date = date
+    g_author = author
+    #wait()
 
 
 f_in.close()
